5248cac161d6d1e99b076908d63ae97500f07849
Sangeetha <an.sangeetha@gmail.com>
2023-12-15 20:59:14 +0100

    add behave features and step for observation_location

--- features/steps/observation_location.py
+++ features/steps/observation_location.py
@@ -17,23 +17,17 @@ def step_impl(context):
 def step_impl(context):
     raise NotImplementedError(u'STEP: When the user asks for observation parameters')
 
-
 @then(u'the date is "2023-12-14"')
 def step_impl(context):
     raise NotImplementedError(u'STEP: Then the date is "2023-12-14"')
-
 
 @then(u'the time is "22:00:00"')
 def step_impl(context):
     raise NotImplementedError(u'STEP: Then the time is "22:00:00"')
 
-
-
 @then(u'the latitude is 40.7128 degree')
 def step_impl(context):
     raise NotImplementedError(u'STEP: Then the latitude is 40.7128 degree')
-
-
 
 @then(u'the longitude is -74.0060 degree')
 def step_impl(context):
